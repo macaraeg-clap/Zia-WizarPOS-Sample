6f1900ee1cfff33e1fa6c71abf2b4ed857f1c8bd
Jimmy Macaraeg <jimmy.macaraeg@pmti.local>
2017-12-13 17:05:15 +0800

    code cleanup

--- WizarSample/app/src/main/java/com/cloudpos/scanserver/aidl/ScanResult.java
+++ WizarSample/app/src/main/java/com/cloudpos/scanserver/aidl/ScanResult.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package com.cloudpos.scanserver.aidl;
 
 import android.graphics.Bitmap;
@@ -15,20 +12,12 @@ import java.io.UnsupportedEncodingException;
 
 public class ScanResult implements Parcelable {
 
-	public final static int ERROR_UNKNOWN = -999;
-	public final static int SCAN_SUCCESS = 1;
-	public final static int CUSTOM_CANCEL = 0;
-	public final static int ERROR_OPEN_AGAIN = -1;
-	public final static int ERROR_DEVICE_OPEN_FAILED = -2;
-	public final static int ERROR_SCAN_TIME_OUT = -3;
-	public final static int ERROR_PARAMETER = -4;
-
-	private int resultCode = ERROR_UNKNOWN;
-	private byte[] rawBuffer = null;
+	public final static int ERROR_UNKNOWN = -999, SCAN_SUCCESS = 1, CUSTOM_CANCEL = 0, ERROR_OPEN_AGAIN = -1, ERROR_DEVICE_OPEN_FAILED = -2, ERROR_SCAN_TIME_OUT = -3,
+			ERROR_PARAMETER = -4;
 	private Bitmap decodeBitmap = null;
-	private String text = null;
-	private String barcodeFormat = null;
-	
+	private String text = null, barcodeFormat = null;
+    private int resultCode = ERROR_UNKNOWN;
+    private byte[] rawBuffer = null;
 	
 	public int getResultCode() {
 		return resultCode;
@@ -64,86 +53,91 @@ public class ScanResult implements Parcelable {
 		this.barcodeFormat = barcodeFormat;
 		try {
 			this.text = new String(rawBuffer, "utf-8");
-		} catch (UnsupportedEncodingException e) {
+		}
+		catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
 	}
 
-	private static final String KEY_RAW_BUFFER = "raw_buffer";
-	private static final String KEY_BITMAP = "bitmap_buffer";
+	private static final String KEY_RAW_BUFFER = "raw_buffer", KEY_BITMAP = "bitmap_buffer";
+
 	public ScanResult(Parcel source) {
 		this.resultCode = source.readInt();
-		if(this.resultCode == SCAN_SUCCESS){
+		if (this.resultCode == SCAN_SUCCESS) {
 			Bundle bundle = source.readBundle();
 			this.rawBuffer = bundle.getByteArray(KEY_RAW_BUFFER);
 			try {
 				this.text = new String(rawBuffer, "utf-8");
-			} catch (UnsupportedEncodingException e) {
+			}
+			catch (UnsupportedEncodingException e) {
 				e.printStackTrace();
 			}
 			byte[] tempBuffer = bundle.getByteArray(KEY_BITMAP);
-			if(tempBuffer != null){
+			if (tempBuffer != null)
 				this.decodeBitmap = convertBuffer2Bitmap(tempBuffer);
-			}
 		}
-		
 	}
+
 	@Override
     public void writeToParcel(Parcel dest, int flags) {
 		dest.writeInt(this.resultCode);
-		if(this.resultCode == SCAN_SUCCESS){
+		if (this.resultCode == SCAN_SUCCESS) {
 			Bundle bundle = new Bundle();
 			bundle.putByteArray(KEY_RAW_BUFFER, this.rawBuffer);
-			if(this.decodeBitmap != null){
+			if (this.decodeBitmap != null) {
 				byte[] bytes = convertBitmap2Buffer(this.decodeBitmap);
 				bundle.putByteArray(KEY_BITMAP, bytes);
 			}
 			dest.writeBundle(bundle);
 		}
 	}
-	
-	// 添加一个静态成员,名为CREATOR,该对象实现了Parcelable.Creator接口
+
 	public static final Parcelable.Creator<ScanResult> CREATOR = new Parcelable.Creator<ScanResult>() {
+
 		@Override
-		public ScanResult createFromParcel(Parcel source) {// 从Parcel中读取数据，返回RuleItem对象
+		public ScanResult createFromParcel(Parcel source) {
 			return new ScanResult(source);
 		}
+
 		@Override
 		public ScanResult[] newArray(int size) {
 			return new ScanResult[size];
 		}
 	};
 	
-	public String toString(){
-		String debug = String.format("resultCode = %s,\n rawBuffer =%s,\n text = %s,\n format = %s,\n decodeBitmap = %s ", this.resultCode, this.rawBuffer, this.text, this.barcodeFormat, this.decodeBitmap);
-		return debug;
+	public String toString() {
+		return String.format("resultCode = %s,\n rawBuffer =%s,\n text = %s,\n format = %s,\n decodeBitmap = %s ", this.resultCode, this.rawBuffer, this.text, this.barcodeFormat,
+                this.decodeBitmap);
 	}
 	
-	
-	private byte[] convertBitmap2Buffer(Bitmap bitmap){
-		 byte[] result = null;
-		 ByteArrayOutputStream output = new ByteArrayOutputStream();//初始化一个流对象
-       try {
-   		bitmap.compress(CompressFormat.PNG, 100, output);//把bitmap100%高质量压缩 到 output对象里
-   		bitmap.recycle();//自由选择是否进行回收
-           result = output.toByteArray();//转换成功了
-       } catch (Exception e) {
-           e.printStackTrace();
-       } finally{
-       	try {
-       		output.close();
-			} catch (Exception e2) {
+	private byte[] convertBitmap2Buffer(Bitmap bitmap) {
+	    byte[] result = null;
+	    ByteArrayOutputStream output = new ByteArrayOutputStream();
+	    try {
+	        bitmap.compress(CompressFormat.PNG, 100, output);
+	        bitmap.recycle();
+	        result = output.toByteArray();
+	    }
+	    catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	    finally {
+	        try {
+	            output.close();
 			}
-       }
-       return result;
+			catch (Exception e2) {
+	            e2.printStackTrace();
+			}
+	    }
+	    return result;
 	}
-	private Bitmap convertBuffer2Bitmap(byte[] buffer){
-		Bitmap bitmap = BitmapFactory.decodeByteArray(buffer, 0, buffer.length);
-      return bitmap;
+
+	private Bitmap convertBuffer2Bitmap(byte[] buffer) {
+	    return BitmapFactory.decodeByteArray(buffer, 0, buffer.length);
 	}
+
 	@Override
 	public int describeContents() {
 		return 0;
 	}
-
 }
